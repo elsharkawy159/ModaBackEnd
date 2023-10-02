d1be5139039ed03e065b0b74c6365532c0f27cd2
Omar Mohamed Elsayed <elsharkawy159.om@gmail.com>
2023-10-02 07:18:03 +0300

    ص

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   return await mongoose
-    .connect(process.env.DB)
+    .connect(
+      "mongodb+srv://elsharkawy159om:I0e69oED9pjXcj5G@moda.lwgbtxr.mongodb.net/"
+    )
     .then(console.log("DB Connection established"))
     .catch((err) => console.log(` Fail to connect  DB.........${err} `));
 };
