f42f28f35dc04a265ab38e67c715fba4d80002c8
Omar Mohamed Elsayed <111247064+elsharkawy159@users.noreply.github.com>
2023-11-07 22:04:28 +0200

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -3,7 +3,7 @@ mongoose.set("strictQuery", false);
 const connectDB = async () => {
   return await mongoose
     .connect(
-      "mongodb+srv://elsharkawy159om:i8Kn85BKTqYlrAeX@cluster0.c4q4sop.mongodb.net/"
+      "mongodb+srv://elsharkawy159om:I0e69oED9pjXcj5G@moda.lwgbtxr.mongodb.net/"
     )
     .then(console.log("DB Connection established"))
     .catch((err) => console.log(` Fail to connect  DB.........${err} `));
